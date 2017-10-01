c43bf21497e366998998ecefa4d8502d463d0ffb
Albert Yang <32423607+GNSPhenomena@users.noreply.github.com>
2017-10-01 00:26:30 -0700

    Update RGBLed6.2_changeValue_function_opt.c

--- examples/RGBLed6.2_changeValue_function_opt.c
+++ examples/RGBLed6.2_changeValue_function_opt.c
@@ -14,11 +14,9 @@ void writeValue(int redValue,int greenValue,int blueValue){
 }
 
 void loop(){
-  for(int i = 0; i<3; i++)
+  for(int i = 0; i < 3; i++)
     for(int j = 0; j < 256; j++)
-    {
       if(i == 0)  writeValue(j,0,0);
       else if(i == 1) writeValue(0,j,0);
       else if(i == 2) writeValue(0,0,j);
-    }
 }
